c2f688b16df28b221a3583e6e481dd47c39b4edb
Matt Eva <matteweva@gmail.com>
2024-03-28 16:48:05 -0700

    set up read-more button to toggle subsequent content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const readMoreButtons = document.getElementsByClassName("read-more");
 
+for (const button of readMoreButtons) {
+  button.addEventListener("click", (e) => {
+    const nextSibling = e.target.nextElementSibling;
+    nextSibling.classList.toggle("hidden-content");
+  });
+}
