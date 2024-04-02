34a1de353955232d41336a1a34dbdf4c8a2b030f
Matt Eva <matteweva@gmail.com>
2024-04-02 16:04:35 -0700

    added content for work experience and project details
    initial draft complete

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ for (const button of readMoreButtons) {
   button.addEventListener("click", (e) => {
     const nextSibling = e.target.nextElementSibling;
     nextSibling.classList.toggle("hidden-content");
+    if (e.target.textContent === "read more") {
+      e.target.textContent = "hide";
+    } else if (e.target.textContent === "hide") {
+      e.target.textContent = "read more";
+    }
   });
 }
